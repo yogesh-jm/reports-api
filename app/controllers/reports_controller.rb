271a7b89a6427291939f1c3219fac3d4369c81ef
yogesh-jm <yogsh.jmv@gmail.com>
2025-04-13 12:26:21 +0530

    Add routes and controller

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,65 @@
+class ReportsController < ApplicationController
+    before_action :ensure_report_presence!, only: %i[show update destroy]
+
+  def index
+    reports = Report.all
+
+    if reports.any?
+      render json: reports, status: :ok
+    else
+      render json: { error: "No reports found"  }, status: :not_found
+    end
+  end
+
+  def show
+    if report
+      render json: report, status: :ok
+    else
+      render json: { error:  "Report not found" }, status: :not_found
+    end
+  end
+
+  def create
+    report = Report.new(report_params)
+
+    if report.save
+      render json: report, status: :created
+    else
+      render json: { errors: report.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if report.update(report_params)
+      render json: report, status: :ok
+    else
+      render json: { errors: report.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    report.destroy
+    render json: { message: "Report successfully deleted" }, status: :no_content
+  end
+
+  private
+
+  def ensure_report_presence!
+    :not_found if report.blank?
+  end
+
+  def report
+    report_id = params[:id].to_i
+
+    if report_id <= 0
+      render json: { error: "Invalid ID" }, status: :unprocessable_entity
+      return
+    end
+
+    @_report ||= Report.find_by(id: report_id)
+  end
+
+  def report_params
+    params.require(:report).permit(:title, :description, :reporter)
+  end
+end
